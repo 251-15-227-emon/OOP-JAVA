a79c43ed3173ce55215e210360cb593083b3d3ca
251-15-227-emon <251-15-227@diu.edu.bd>
2026-07-09 18:40:23 +0600

    Added MathFunction package

---
+++ src/main/java/MathFunction/Example.java
@@ -0,0 +1,36 @@
+package MathFunction;
+
+public class Example {
+
+    public static void main(String[] args) {
+
+        //positive value
+        System.out.println(Math.abs(-9.5));
+
+        //square root
+        System.out.println(Math.sqrt(25));
+
+        //power
+        System.out.println(Math.pow(2, 3));
+
+        //PI
+        System.out.println(Math.PI);
+
+        //Log()
+        System.out.println(Math.log(1));
+
+        //Exponential
+        System.out.println(Math.exp(1));
+
+        //MaxValue
+        System.out.println(Math.max(10, 9999));
+
+        //MinValue
+        System.out.println(Math.min(2.4, -9.0));
+
+        System.out.println(Math.ceil(-8.8));
+        System.out.println(Math.floor(5.5));
+
+    }
+
+}
