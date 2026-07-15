cf96d40b825ac69b1457c2eed0aecc35fe97cab2
251-15-227-emon <251-15-227@diu.edu.bd>
2026-07-15 22:28:12 +0600

    Added For_Each_Loop to Array package

---
+++ src/main/java/Array/For_Each_Loop.java
@@ -0,0 +1,21 @@
+package Array;
+
+public class For_Each_Loop {
+
+    public static void main(String[] args) {
+
+        String[] name = {"Emon", "Anika", "Promit", "Afia", "Samiul"};
+
+        int[] number = {5, 6, 7, 8};
+
+        for (int x : number) {
+            System.out.println(x);
+        }
+
+        for (String y : name) {
+
+            System.out.println(y);
+        }
+    }
+
+}
