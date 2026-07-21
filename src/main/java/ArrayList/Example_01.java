6e931eaa1618b5b3d659631932cc7fdeca52077a
251-15-227-emon <251-15-227@diu.edu.bd>
2026-07-21 12:03:29 +0600

    Added ArraList Package

---
+++ src/main/java/ArrayList/Example_01.java
@@ -0,0 +1,38 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Example_01 {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> number = new ArrayList<>();
+
+        System.out.println("Size =" + number.size());
+
+        number.add(10);
+        number.add(20);
+        number.add(2, 40);
+
+        System.out.println(number);
+        
+        for(int x: number){
+        
+            System.out.print("   "+x);
+        
+        }
+        
+        
+        // Iterator itr =number.iterator();
+        
+     /* while(itr.hasNext()){
+       
+           System.out.print(" "+itr.next());
+       
+       }*/
+        
+        System.out.println(" Size =" + number.size());
+
+    }
+
+}
