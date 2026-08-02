344e6274b5d9cfc94fdc86c6a9ce7a75f81c4c70
251-15-227-emon <251-15-227@diu.edu.bd>
2026-08-02 23:28:20 +0600

    Added Example_02 to ArrayList package

---
+++ src/main/java/ArrayList/Example_02.java
@@ -0,0 +1,19 @@
+package ArrayList;
+import java.util.ArrayList;
+
+public class Example_02 {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+
+        System.out.println(numbers);
+
+    }
+
+}
+    
