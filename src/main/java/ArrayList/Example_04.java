d0563eedd418324b6cbfa2d8e00b045feef0ec56
251-15-227-emon <251-15-227@diu.edu.bd>
2026-08-19 00:37:42 +0600

    Update Example_04.java

--- src/main/java/ArrayList/Example_04.java
+++ src/main/java/ArrayList/Example_04.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ArrayList;
 
-/**
- *
- * @author EMON
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Example_04 {
+    
+    
+    public static void main(String[] args) {
+        
+        
+       List <String> name = new ArrayList<>(); 
+        
+       name.add("Emon");
+       name.add("Anika");
+       name.add("Promit");
+       
+        System.out.println("List of Name :");
+        
+        for(String n :name){
+        
+            System.out.println(n);
+        
+        }
+        
+        // Access specific Index
+        
+        System.out.println("Second Name :"+name.get(1));
+        
+        // Remove an element 
+        
+        name.remove("Promit");
+        
+        System.out.println("List After Removal :");
+        
+        for(String n : name){
+        
+            System.out.println(n);
+        }
+        
+        
+    }
     
 }
