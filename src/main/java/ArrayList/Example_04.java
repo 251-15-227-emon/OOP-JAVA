c6faa4edb857f7c28d545efdb3934171eb2ec13c
251-15-227-emon <251-15-227@diu.edu.bd>
2026-08-19 01:12:08 +0600

    updated

--- src/main/java/ArrayList/Example_04.java
+++ src/main/java/ArrayList/Example_04.java
@@ -3,43 +3,37 @@ package ArrayList;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Example_04 {
-    
-    
+
     public static void main(String[] args) {
-        
-        
-       List <String> name = new ArrayList<>(); 
-        
-       name.add("Emon");
-       name.add("Anika");
-       name.add("Promit");
-       
+
+        List<String> name = new ArrayList<>();
+
+        name.add("Emon");
+        name.add("Anika");
+        name.add("Promit");
+
         System.out.println("List of Name :");
-        
-        for(String n :name){
-        
+
+        for (String n : name) {
+
             System.out.println(n);
-        
+
         }
-        
+
         // Access specific Index
-        
-        System.out.println("Second Name :"+name.get(1));
-        
+        System.out.println("Second Name :" + name.get(1));
+
         // Remove an element 
-        
         name.remove("Promit");
-        
+
         System.out.println("List After Removal :");
-        
-        for(String n : name){
-        
+
+        for (String n : name) {
+
             System.out.println(n);
         }
-        
-        
+
     }
-    
+
 }
--- src/main/java/HashSet/Emon.java
+++ src/main/java/HashSet/Emon.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package HashSet;
 
-/**
- *
- * @author EMON
- */
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Emon {
-    
+
+    public static void main(String[] args) {
+
+        List<String> names = new ArrayList<>();
+
+        names.add("Emon");
+        names.add("Anika");
+        names.add("Promit");
+        names.add("Emon");
+        System.out.println("Hashset Name : ");
+
+        for (String n : names) {
+
+            System.out.println(n);
+        }
+
+        Set<String> uniqueNames = new HashSet<>();
+
+        System.out.println("Duplicate Names :");
+
+        for (String name : names) {
+
+            if (!uniqueNames.add(name)) {
+
+                System.out.println("Duplicate detected: " + name);
+
+            }
+        }
+
+        System.out.println("Total Distinct Names: " + uniqueNames.size());
+
+        System.out.println("Distinct Names:");
+
+        for (String name : uniqueNames) {
+
+            System.out.println(name);
+        }
+
+    }
 }
